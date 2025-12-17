855172eb97b49cd67b4d73ad603b8d7e8acdb273
thedainc23 <132509636+thedainc23@users.noreply.github.com>
2025-12-18 05:03:40 +0800

    Update Firebase admin service account configuration 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,18 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = process.env.GCLOUD_SECRET_KEY;
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "dreamcatchers-test",
+  "private_key_id": process.env.GCSA_PRIVATE_KEY_ID,
+  "private_key": process.env.GCSA_PRIVATE_KEY,
+  "client_email": process.env.GCSA_CLIENT_EMAIL,
+  "client_id": process.env.GCSA_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40dreamcatchers-test.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
